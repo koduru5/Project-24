176231077d670ed715012237fa0ce4fbe1b3e43f
koduru5 <68308123+koduru5@users.noreply.github.com>
2020-08-25 13:14:53 -0400

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,16 @@
+class Box {
+    constructor(x, y, width, height) {
+      this.body = Bodies.rectangle(x, y, width, height);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("white");
+      fill(250);
+      rect(0, 0, this.width, this.height);
+    }
+  };
---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function setup() {
+	createCanvas(800, 700);
+
+	ground = new Ground(400,height,800,20);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	box1= new Dustbin(440, 650, 200, 20);
+	box2= new Dustbin(370,610,20,100);
+	box3= new Dustbin(550,610,20,100);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if(keyCode === UP_ARROW) {
+		Matter.Body.applyForce(PaperObject.body,paperObject.body.position,{x:85,y:-85});
+	}
+}
+
+
+
